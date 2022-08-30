14d485e2eff8bb0e6c04b84e51a89a0fc2693cac
Fadlan Amrullah <51012056+fadlanam30@users.noreply.github.com>
2022-08-30 15:40:32 +0000

    appmain

--- App.js
+++ App.js
@@ -87,7 +87,7 @@ const App = () => {
             Button State Example
           </Text>
           <Text style={[styles.sectionDescription]}>
-            I am GooGooo, and I am {isHungry ? "hungry" : "kenyang"}!    
+            I am FFFFF, and I am {isHungry ? "hungry" : "kenyang"}!    
           </Text>
           <Button 
               onPress={() => {
